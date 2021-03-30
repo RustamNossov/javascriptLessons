707edabe4b0b4d1af826d5c34986c12ce4670d31
RustamNossov <rustamn48@gmail.com>
2021-03-30 17:24:09 +0600

    first commit

---
+++ firstLesson/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// let answers = {};
+// answers.name = prompt('What is your name?', '');
+// answers.sorname = prompt('What is sorname name?', '');
+// answers.age = prompt('What is your age', '');
+
+// console.log(answers);
+
+
+const name = 'Rus';
+alert(`Hello ${name}!`)
