9cafa4158e288c0d45eb93418f8e25490329580d
RustamNossov <rustamn48@gmail.com>
2021-03-31 18:42:12 +0600

    First task

--- firstLesson/js/script.js
+++ firstLesson/js/script.js
@@ -8,5 +8,23 @@
 // console.log(answers);
 
 
-const name = 'Rus';
-alert(`Hello ${name}!`)
+const numberOfFilms = +prompt('How many movies have you seen?', '');
+let personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+const a = prompt('One of the last movies you have seen?', ''),
+      b = +prompt('How you score it?', ''),
+      c = prompt('One of the last movies you have seen?', ''),
+      d = +prompt('How you score it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
